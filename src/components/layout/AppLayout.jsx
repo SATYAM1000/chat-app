28c88263625232f78f7883ac93c12b87a5c75dc9
satyamx <satyamsingh748846@gmail.com>
2024-03-31 00:47:32 +0530

    new

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material";
 import Title from "../shared/Title";
 import Header from "./Header";
 
@@ -10,8 +11,37 @@ const AppLayout = () => (WrappedComponent) => {
           description="This is chat application"
         />
         <Header />
+        <Grid container height={"calc(100vh - 4rem)"}>
+          <Grid
+            item
+            sm={4}
+            md={3}
+            sx={{
+              display: { xs: "none", sm: "block" },
+            }}
+            height={"100%"}
+          >
+            FIRST
+          </Grid>
+          <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
+            <WrappedComponent {...props} />
+          </Grid>
+          <Grid
+            item
+            md={4}
+            lg={3}
+            height={"100%"}
+            sx={{
+              display: { xs: "none", md: "block" },
+              padding: "2rem",
+              bgcolor: "rgba(0,0,0,0.85)",
+            }}
+          >
+            THIRD
+          </Grid>
+        </Grid>
+
         <WrappedComponent {...props} />
-        <div>Footer</div>
       </>
     );
   };
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,47 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { orange } from "../../constants/color";
+import { Menu as MenuIcon } from "@mui/icons-material";
 
 const Header = () => {
+  const handleMobile = () => {};
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar
+          position="static"
+          sx={{
+            bgcolor: orange,
+          }}
+        >
+          <Toolbar>
+            <Typography
+              variant="h6"
+              sx={{
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              Chat App
+            </Typography>
+            <Box
+              sx={{
+                display: { xs: "block", sm: "none" },
+              }}
+            >
+              <IconButton color="inherit" onClick={handleMobile}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Box
+              sx={{
+                flexGrow: 1,
+              }}
+            />
+            <Box>Final</Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
 
-export default Header
+export default Header;
